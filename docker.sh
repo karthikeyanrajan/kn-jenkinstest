1e1fe3af2e7e97797cace57b1147226986a36d1e
root <root@ip-172-31-21-158.ec2.internal>
2024-02-12 17:25:55 +0000

    new

---
+++ docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable Docker repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update the apt package index again, and install the latest version of Docker CE and containerd
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the Docker Compose binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Create a symbolic link to /usr/bin
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Display Docker and Docker Compose versions
+docker --version
+docker-compose --version
+
